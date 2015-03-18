f7471ea65d25749398bafd554cf6928a0f46b7df
aerotog <aerotog@gmail.com>
2015-03-17 19:12:59 -0500

    Finish object creation

--- TOGPyJuggle.py
+++ TOGPyJuggle.py
@@ -1,77 +1,82 @@
 #!/usr/bin/env python
 
 class Circuit():	
-	def __init__(self, data):
-		#print(data)
-		name = data[0]
-		stats_list = data[1:]
-		stats_dict = {}
-		# Convert stats raw list to dictionary
-		for stat in stats_list:
-			k,v = stat.split(":")
-			stats_dict[k] = int(v)
 
-		print(name)
-		print(stats_dict)
+	def __init__(self, data):
+
+		self.name = data[0]
+		self.stats_list = data[1:]
+		self.stats_dict = {}
+
+		# Convert stats raw list to dictionary
+		for stat in self.stats_list:
+			k,v = stat.split(":")
+			self.stats_dict[k] = int(v)
 
 
 class Juggler():
-	def __init__(self, data):
-		#print(data)
-		name = data[0]
-		stats_list = data[1:4]
-		circuits_wanted = data[4:]
-		stats_dict = {}
-		# Convert stats raw list to dictionary
-		for stat in stats_list:
-			k,v = stat.split(":")
-			stats_dict[k] = int(v)
 
-		print(name)
-		print(stats_dict)
-		print(circuits_wanted)
+	def __init__(self, data, circuits):
+
+		self.name = data[0]
+		self.stats_list = data[1:4]
+		self.circuits_wanted = data[4].split(",")
+		self.perf_stats = {}
+
+		# Convert stats raw list to dictionary
+		self.stats_dict = {}		
+		for stat in self.stats_list:
+			k,v = stat.split(":")
+			self.stats_dict[k] = int(v)
+
+		# Populate performance values for desired circuits
+		for cw_key in self.circuits_wanted:
+			value = 0
+			for sd_key in self.stats_dict:
+				value += circuits[cw_key].stats_dict[sd_key] * self.stats_dict[sd_key]
+
+			self.perf_stats[cw_key] = value
+
+		print(self.perf_stats)
+
+	def performance(self, circuits):
+
+		for cw_key in self.circuits_wanted:
+			value = 0
+
+			for sd_key in self.stats_dict:
+				value += circuits[cw_key].stats_dict[sd_key] * self.stats_dict[sd_key]
+
+			self.perf_stats[cw_key] = value
+
+		print(self.perf_stats)
+
 
 def file_to_list(file_name):
-	juggfest_data = open(file_name, "r")
 
+	juggfest_data = open(file_name, "r")
 	line = juggfest_data.readline()
-	jugglers = []
-	circuits = []
+	
+	global Circuits
+	global Jugglers
 	
 	while line:
 		info = line.split()
-		#print(info)
 		if len(info) > 0:	
-			#print(info[0])
 
 			if info[0] == "C":
-				#print(info[1:])
-
 				temp = Circuit(info[1:])
-				circuits.append(temp)
+				Circuits[temp.name] = temp
 
-			if info[0] == "J":
-				#print(info[1:])
-
-				temp = Juggler(info[1:])
-				jugglers.append(temp)
-
-
-		"""
-		if info[0] == "C":
-			Temp = Circuit()
-			circuits.append(Tem
-
-		if info[0] == "J":
-			tempo = Juggler()
-			circuits.append(Temp)
-		"""
-		#print(circuits)
-		#print(jugglers)
-
+			elif info[0] == "J":
+				temp = Juggler(info[1:], Circuits)
+				Jugglers[temp.name] = temp
 
 		line = juggfest_data.readline()
 
-	return 0
+
+Circuits = {}
+Jugglers = {}
 
 file_to_list("JUGGLE_TEST.txt")
+
